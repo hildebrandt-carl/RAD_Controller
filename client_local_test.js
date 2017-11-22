f02bc75b3005a7c5fd971efb2fd316ffa3366e6b
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2017-11-22 14:58:58 -0600

    Added two test files. The first tests local servers and the second tests non local servers

--- client_local_test.js
+++ client_local_test.js
@@ -1,9 +1,9 @@
 // client
 var s = require('net').Socket();
 
-s.connect(4242,'160.119.248.176');
+s.connect(4242,'127.0.0.1');
 
-s.write('Web');
+s.write('Hello');
 
 s.on('data', function(d){
     console.log(d.toString());
