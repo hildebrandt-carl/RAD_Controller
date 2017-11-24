c966914b743a8d5c8e8e116560d8b64e37f69fc8
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2017-11-24 08:12:08 -0600

    The small updates

--- client_com_test.js
+++ client_com_test.js
@@ -3,28 +3,10 @@ var s = require('net').Socket();
 
 s.connect(4242,'160.119.248.176');
 
-s.write('web');
-
-var i = 0;
+s.write('controller');
 
 s.on('data', function(d){
     console.log(d.toString());
-    i = i + 1;
-    if(i==1)
-    {
-        console.log("sending first message");
-        s.write('firstMessage');
-    }
-
-    if(i==2)
-    {
-        console.log("sending second message");
-        s.write('secondMessage');
-    }
-
-    if(i >= 3)
-    {
-        s.end();
-    }
-
 });
+
+s.end();
