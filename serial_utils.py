36da453ab9b821510c2a76f2cc7a1ef525e3c934
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2017-12-08 15:34:01 -0600

    Added the serial code to the controller

--- serial_utils.py
+++ serial_utils.py
@@ -11,12 +11,12 @@ def getConnected():
 	return connected
 
 @timeout(10)
-def connect():
+def connect(inPort):
 	global connected
 	global portName
 	global port
 	connected = False
-	portName = '/dev/ttyS0'
+	portName = inPort
 	try:
 		port = serial.Serial(portName)
 		port.timeout = 1
@@ -24,7 +24,8 @@ def connect():
 		connected = True
 		return
 	except (OSError, serial.SerialException):
-		pass
+		print("Connection failure")
+		exit(0)
 	return
 
 def disconnect(*args):
